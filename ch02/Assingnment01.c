082eedf7ac6a8791c5fdbdf19539dae0d1a57a73
rladbsqls <qls252@naver.com>
2025-03-22 21:45:03 +0900

    초기 커밋.

---
+++ ch02/Assingnment01.c
@@ -0,0 +1,20 @@
+//파일명:assingnment01.c
+//작성자: 김윤빈
+//날짜: 2025-03-22
+//버전:v1.0
+
+#include <stdio.h>
+
+void Printf(void);
+
+int main()
+{
+	Printf();
+	return 0;
+}
+
+void Printf(void)
+{
+	printf("int main(void)\{\    return 0;\}");
+	return;
+}
